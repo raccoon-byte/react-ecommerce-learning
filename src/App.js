8bf13ba3b54e053efcc9366beb30e5a18aeb8845
Seth D <73028082+raccoon-byte@users.noreply.github.com>
2022-06-13 09:06:43 -0500

    category-item component reworked into a proper component.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import CategoryItem from "./components/category-item/category-item.component";
+
 const App = () => {
   const categories = [
     {
@@ -28,18 +30,9 @@ const App = () => {
   ];
 
   return (<div className="categories-container">
-    {categories.map(({ id, title, imageUrl }) => (
-      <div key={id} className="category-container">
-        <div className="background-image"
-             style={{
-               backgroundImage: `url(${imageUrl})`
-             }}
-        />
-        <div className="category-body-container">
-          <h2>{title}</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>))}
+    {categories.map((category) => (
+      <CategoryItem key={category.id} category={category} />
+    ))}
   </div>);
 };
 
