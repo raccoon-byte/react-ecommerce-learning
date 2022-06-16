82198f777a8722682891e41af433500559bc7a07
Seth D <73028082+raccoon-byte@users.noreply.github.com>
2022-06-16 11:16:51 -0500

    first portion of Firebase config file done.

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+
+// The web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBBwi4o1cwpgs_DOVMCx-yhiBBNKobYWnw",
+  authDomain: "react-learning-db-6bd2b.firebaseapp.com",
+  projectId: "react-learning-db-6bd2b",
+  storageBucket: "react-learning-db-6bd2b.appspot.com",
+  messagingSenderId: "395482290171",
+  appId: "1:395482290171:web:dbe9bbb928100054eedd5e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
