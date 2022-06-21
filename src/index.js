44df4a5b0884cff440f86de2b1fa571cd04e3bc5
Seth D <73028082+raccoon-byte@users.noreply.github.com>
2022-06-21 18:22:28 -0500

    added cart context. adding shopping icon and cart dropdown with toggling functionality.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from "./contexts/user.context";
 import { ProductsProvider } from "./contexts/products.context";
+import { CartProvider } from "./contexts/cart.context";
 
 import './index.scss';
 
@@ -14,7 +15,9 @@ root.render(
     <BrowserRouter>
       <UserProvider>
         <ProductsProvider>
-          <App/>
+          <CartProvider>
+            <App/>
+          </CartProvider>
         </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,21 +1,26 @@
 import { Fragment, useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 
-import {ReactComponent as CrownLogo} from '../../assets/crown.svg';
-import {UserContext} from "../../contexts/user.context";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
+import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
+
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 import './navigation.styles.scss';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <Fragment>
       <div className='navigation'>
         <Link className='logo-container' to='/'>
-          <CrownLogo className='logo' />
+          <CrownLogo className='logo'/>
         </Link>
         <div className='nav-links-container'>
           <Link className='nav-link' to='/shop'>
@@ -31,7 +36,9 @@ const Navigation = () => {
                 SIGN IN
               </Link>
             )}
+          <CartIcon />
         </div>
+        {isCartOpen && (<CartDropdown />)}
       </div>
       <Outlet/>
     </Fragment>
