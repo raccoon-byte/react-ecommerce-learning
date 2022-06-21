9851969ea8f1b77db066297625a42fe0e6af4558
Seth D <73028082+raccoon-byte@users.noreply.github.com>
2022-06-21 00:29:47 -0500

    Observer auth listener set up and centralized.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,18 +4,12 @@ import { Link, Outlet } from 'react-router-dom';
 import {ReactComponent as CrownLogo} from '../../assets/crown.svg';
 import {UserContext} from "../../contexts/user.context";
 
-import {signOutUser} from "../../utils/firebase/firebase.utils";
+import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 import './navigation.styles.scss';
 
 const Navigation = () => {
-  const { currentUser, setCurrentUser } = useContext(UserContext);
-
-  const signOutHandler = async () => {
-    const resp = await signOutUser();
-    setCurrentUser(null);
-    console.log(resp);
-  }
+  const { currentUser } = useContext(UserContext);
 
   return (
     <Fragment>
@@ -29,7 +23,7 @@ const Navigation = () => {
           </Link>
           {
             currentUser ? (
-              <span className='nav-link' onClick={signOutHandler}>
+              <span className='nav-link' onClick={signOutUser}>
                 SIGN OUT
               </span>
             ) : (
