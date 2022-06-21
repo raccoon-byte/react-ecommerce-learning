d557f524f3432d602f84125be9ec322648ab62d1
Seth D <73028082+raccoon-byte@users.noreply.github.com>
2022-06-21 17:21:44 -0500

    products context implemented.

---
+++ src/contexts/products.context.jsx
@@ -0,0 +1,14 @@
+import { createContext, useState } from "react";
+
+import PRODUCTS_DATA from '../shop-data.json';
+
+export const ProductsContext = createContext({
+  products: [],
+});
+
+export const ProductsProvider = ({ children }) => {
+  const [products, setProducts] = useState(PRODUCTS_DATA);
+  const value = { products };
+
+  return <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>;
+}
