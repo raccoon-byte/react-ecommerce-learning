d088d6fa016bf4d4d4299aef3a65e4c1e54c56e4
Seth D <73028082+raccoon-byte@users.noreply.github.com>
2022-06-20 23:53:52 -0500

    Sign in / sign out navbar changes and signout onclick implemented.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,11 +4,18 @@ import { Link, Outlet } from 'react-router-dom';
 import {ReactComponent as CrownLogo} from '../../assets/crown.svg';
 import {UserContext} from "../../contexts/user.context";
 
+import {signOutUser} from "../../utils/firebase/firebase.utils";
+
 import './navigation.styles.scss';
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
-  console.log(currentUser);
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+
+  const signOutHandler = async () => {
+    const resp = await signOutUser();
+    setCurrentUser(null);
+    console.log(resp);
+  }
 
   return (
     <Fragment>
@@ -20,9 +27,16 @@ const Navigation = () => {
           <Link className='nav-link' to='/shop'>
             SHOP
           </Link>
-          <Link className='nav-link' to='/auth'>
-            SIGN IN
-          </Link>
+          {
+            currentUser ? (
+              <span className='nav-link' onClick={signOutHandler}>
+                SIGN OUT
+              </span>
+            ) : (
+              <Link className='nav-link' to='/auth'>
+                SIGN IN
+              </Link>
+            )}
         </div>
       </div>
       <Outlet/>
